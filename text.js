84f1c22b432613e19299257ff4c58cfb2c522f6f
telluhh_dev <71474910+Thanaxx@users.noreply.github.com>
2021-05-20 10:04:29 +0800

    Update text.js

--- text.js
+++ text.js
@@ -1,4 +1,7 @@
 function run(x, y){
     return x * y;
+    
+//     new change
+    alert("we change for a reson, commit changes");
 }
     document.getElementById('new').innerHTML = run(1000,655783);
